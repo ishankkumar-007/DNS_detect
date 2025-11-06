43d1fc442d9f1f219eccfca6fcb63f9397b2c5fb
ishankkumar-007 <ishankkumar123@gmail.com>
2025-11-07 03:31:36 +0530

    QoL: dump unnecessary files

--- src/evaluation/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Evaluation Package
-Metrics, visualizations, and model comparison utilities
-"""
-
-__all__ = []
--- src/trainers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Model Trainers Package
-Training orchestration for different model types
-"""
-
-__all__ = []
